bc3b611b1d75b9affa56652e9d499189932ea4c6
ab1nv <abnvxsin@gmail.com>
2024-09-25 10:40:22 +0530

    Improve file structure

--- cses-problemset/weird_algorithm.cpp
+++ cses-problemset/weird_algorithm.cpp
@@ -1,25 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
 
-    long long n;
-    cin >> n;
+  long long n;
+  cin >> n;
 
-    cout << n << " ";
+  cout << n << " ";
 
-    while (n != 1)
-    {
-        if (n % 2 == 0)
-        {
-            n /= 2;
-            cout << n << " ";
-        }
-        else
-        {
-            n = (n * 3) + 1;
-            cout << n << " ";
-        }
+  while (n != 1) {
+    if (n % 2 == 0) {
+      n /= 2;
+      cout << n << " ";
+    } else {
+      n = (n * 3) + 1;
+      cout << n << " ";
     }
+  }
 }
--- templates/basic.cpp
+++ templates/basic.cpp
@@ -4,6 +4,5 @@ using namespace std;
 #define str string
 #define ll long long
 #define all(x) (x).begin(), (x).end()
-#define f(i, c, n) for (i = (c); i < (n); i++)
 
 int main() {}
